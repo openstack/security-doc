0badc7dd7ca0e7916f30b506f573f028084f99e8
Andreas Jaeger <aj@suse.de>
2015-10-14 08:56:49 +0200

    Do not copy sources
    We do not need to copy source files anymore with current
    openstackdocstheme, unset html_show_sourcelink and html_copy_source
    links. Report a bug works without these as well.
    
    This reduces the amount of files we build and publish (for Install Guide
    from 42 MB to 39 MB), and thus speeds up building (from 44 to 41s for
    Install Guide) slightly.
    
    Change-Id: I7990c62db1c58c4cc30dc29c2b4ed1822048d531

--- security-guide/source/conf.py
+++ security-guide/source/conf.py
@@ -179,8 +179,7 @@ html_use_index = False
 # html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-# This one is needed for "Report a bug".
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 # html_show_sphinx = True
@@ -199,6 +198,8 @@ html_use_index = False
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'security-guide'
 
+# If true, publish source files
+html_copy_source = False
 
 # -- Options for LaTeX output ---------------------------------------------
 

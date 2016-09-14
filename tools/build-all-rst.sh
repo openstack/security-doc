720a9db05d56e6ab5a46f22a648a7344317be7a1
Andreas Jaeger <aj@suse.com>
2016-09-14 20:50:23 +0200

    Add marker files
    Add marker files in the root of each document.
    
    This is needed for publishing in the future:
    This is so that we can create a list of directories to ignore when we
    rsync documentation builds to a publishing site. Some builds are
    published as subdirectories underneath the results of superior builds.
    
    This allows those superior builds to be rsynced without removing the
    other builds that were published under them.
    
    Change-Id: Id0f63749e767c9e9db9619d2cb9320b1a20f5995

--- tools/build-all-rst.sh
+++ tools/build-all-rst.sh
@@ -2,7 +2,12 @@
 
 mkdir -p publish-docs
 
+# This marker is needed for infra publishing
+MARKER_TEXT="Project: $ZUUL_PROJECT Ref: $ZUUL_REFNAME Build: $ZUUL_UUID"
+
 doc-tools-build-rst security-guide --build build \
         --target security-guide
+echo $MARKER_TEXT > publish-docs/security-guide/.root-marker
 doc-tools-build-rst security-threat-analysis --build build \
         --target security-threat-analysis
+echo $MARKER_TEXT > publish-docs/security-threat-analysis/.root-marker

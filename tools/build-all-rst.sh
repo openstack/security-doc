b58eeb352a855838aa6bfbe06179f414465e917d
Andreas Jaeger <aj@suse.com>
2019-08-15 06:33:53 +0200

    Use publish-docs/html
    Publish to new location for documents to simplify our setup.
    
    Depends-On: https://review.opendev.org/676489
    Change-Id: I4adf8b1b90bee71bdfbe11affab6c2d18be1ab06

--- tools/build-all-rst.sh
+++ tools/build-all-rst.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
 
-mkdir -p publish-docs
+mkdir -p publish-docs/html
 
 # This marker is needed for infra publishing
 MARKER_TEXT="Project: $ZUUL_PROJECT Ref: $ZUUL_REFNAME Build: $ZUUL_UUID Revision: $ZUUL_NEWREV"
 
 doc-tools-build-rst security-guide --build build \
-        --target security-guide
-echo $MARKER_TEXT > publish-docs/security-guide/.root-marker
+                    --target security-guide
+# TODO (AJaeger): Remove once openstack-doc-tools is updated
+mv publish-docs/security-guide publish-docs/html
+echo $MARKER_TEXT > publish-docs/html/security-guide/.root-marker
 doc-tools-build-rst security-threat-analysis --build build \
         --target security-threat-analysis
-echo $MARKER_TEXT > publish-docs/security-threat-analysis/.root-marker
+# TODO (AJaeger): Remove once openstack-doc-tools is updated
+mv publish-docs/security-threat-analysis publish-docs/html
+echo $MARKER_TEXT > publish-docs/html/security-threat-analysis/.root-marker
